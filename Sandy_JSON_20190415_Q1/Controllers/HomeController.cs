92d99f3008f6ed88415b975707692749825fbf8e
sandyyeh <sandyy9996@gmail.com>
2019-05-13 15:07:05 +0800

    貨幣格式改用 switch-case 判斷

--- Sandy_JSON_20190415_Q1/Controllers/HomeController.cs
+++ Sandy_JSON_20190415_Q1/Controllers/HomeController.cs
@@ -11,46 +11,22 @@ namespace Sandy_JSON_20190415_Q1.Controllers
     {
         public ActionResult Index()
         {
-           
+
             string file = Server.MapPath("~/App_Data/data.json");
             //deserialize JSON from file  
             string json = System.IO.File.ReadAllText(file);
-
-          
             List<ViewModel> list = JsonConvert.DeserializeObject<List<ViewModel>>(json);
-  
+
             foreach (var item in list)
             {
-              
+
                 if (!string.IsNullOrEmpty(item.Price))
-                {               
+                {
                     if (decimal.TryParse(item.Price, out decimal number))
                     {
                         float price = Convert.ToSingle(item.Price);
-                        if (item.Locale == "US" )
-                        {                          
-                            item.Price = string.Format(new CultureInfo("en-US"), "{0:c}", price);
-                        }
-                        else if (item.Locale == "CA")
-                        {                          
-                            item.Price = string.Format(new CultureInfo("en-CA"), "{0:c}", price);
-                        }                  
-                        else if (item.Locale == "JP")
-                        {
-                            item.Price = string.Format(new CultureInfo("ja-JP"), "{0:c}", price);
-                        }
-                        else if (item.Locale == "ES")
-                        {                     
-                            item.Price = string.Format(new CultureInfo("es-ES"), "{0:c}", price);
-                        }
-                        else if (item.Locale == "DE")
-                        {
-                            item.Price = string.Format(new CultureInfo("de-DE"), "{0:c}", price);
-                        }
-                        else if (item.Locale == "FR")
-                        {
-                            item.Price = string.Format(new CultureInfo("fr-FR"), "{0:c}", price);
-                        }
+                        item.Price = string.Format(new CultureInfo(GetLocale(item.Locale)), "{0:c}", price);
+
                     }
                     else
                     {
@@ -61,51 +37,59 @@ namespace Sandy_JSON_20190415_Q1.Controllers
                 {
                     item.Price = (!string.IsNullOrEmpty(item.Price)) ? item.Price : "-";
                 }
-                    
-
 
 
                 if (!string.IsNullOrEmpty(item.Promote_Price))
-                {                   
+                {
                     if (decimal.TryParse(item.Promote_Price, out decimal number2))
                     {
                         float promtePrice = Convert.ToSingle(item.Promote_Price);
-                     
-                        if (item.Locale == "US")
-                        {
-                            item.Promote_Price = string.Format(new CultureInfo("en-US"), "{0:c}", promtePrice);
-                        }
-                        else if (item.Locale == "CA")
-                        {
-                            item.Promote_Price = string.Format(new CultureInfo("en-CA"), "{0:c}", promtePrice);
-                        }
-                        else if (item.Locale == "JP")
-                        {
-                            item.Promote_Price = string.Format(new CultureInfo("ja-JP"), "{0:c}", promtePrice);
-                        }
-                        else if (item.Locale == "ES")
-                        {
-                            item.Promote_Price = string.Format(new CultureInfo("es-ES"), "{0:c}", promtePrice);
-                        }
-                        else if (item.Locale == "DE")
-                        {
-                            item.Promote_Price = string.Format(new CultureInfo("de-DE"), "{0:c}", promtePrice);
-                        }
-                        else if (item.Locale == "FR")
-                        {
-                            item.Promote_Price=string.Format(new CultureInfo("fr-FR"), "{0:c}", promtePrice);
-                        }
+                        item.Promote_Price = string.Format(new CultureInfo(GetLocale(item.Locale)), "{0:c}", promtePrice);
+
                     }
                     else { item.Promote_Price = item.Promote_Price.Replace(item.Promote_Price, "-"); }
-                
+
                 }
+
                 item.Promote_Price = (!string.IsNullOrEmpty(item.Promote_Price)) ? item.Promote_Price : "-";
             }
 
 
-                return View(list);
+            return View(list);
         }
-          
+
+
+
+        public string GetLocale(string locale)
+        {
+            string result = "";
+
+            switch (locale)
+            {
+                case "US":
+                    result = "en-US";
+                    break;
+                case "CA":
+                    result = "en-CA";
+                    break;
+                case "JP":
+                    result = "ja-JP";
+                    break;
+                case "ES":
+                    result = "es-ES";
+                    break;
+                case "DE":
+                    result = "de-DE";
+                    break;
+                case "FR":
+                    result = "fr-FR";
+                    break;
+            }
+
+
+            return result;
+        }
+
     }
 
 }
